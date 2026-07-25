d677af18b1b025998a05ab8ddd85848e5616cd09
Raghav Garg <raghavgarg@Raghavs-MacBook-Air.local>
2026-07-25 23:51:29 +0530

    implemented market lifecycle

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Wallet,Market,Outcome
 
 
 
@@ -26,5 +27,90 @@ class LoginSerializer(serializers.Serializer):
     )
 
 
+class DepositSerializer(serializers.Serializer):
+    amount = serializers.DecimalField(max_digits=12,decimal_places=2, default=0,min_value=0.01)
 
-   
+
+
+    # def validate_amount(self, value):
+    #     if value <= 0:
+    #         raise serializers.ValidationError(
+    #             "Amount must be greater than 0."
+    #         )
+
+    #     return value
+
+
+
+class WalletSerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = Wallet
+        fields = [
+            "available_balance",
+            "locked_balance"
+        ]
+
+
+class MarketSerializer(serializers.ModelSerializer):
+    class Meta : 
+        model = Market
+        fields = [
+            "title",
+            "description",
+            "close_at"
+        ]
+
+
+class MarketOutputSerializer(serializers.ModelSerializer):
+    class Meta : 
+        model = Market
+        fields = [
+            "id",
+            "title",
+            "description",
+            "status",
+            "close_at"
+        ]
+
+
+class OutcomeSerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = Outcome
+        fields = [
+            "id",
+            "name"
+        ]
+
+class MarketDetailSerializer(serializers.ModelSerializer):
+    outcomes = OutcomeSerializer(many = True,read_only = True)
+
+    class Meta :
+        model = Market
+        fields = [
+            "id",
+            "title",
+            "description",
+            "status",
+            "close_at",
+            "outcomes"
+
+        ]
+
+
+class PlaceOrderSerializer(serializers.ModelSerializer):
+    pass
+
+
+class MergeSerializer(serializers.Serializer):
+
+    quantity = serializers.PositiveIntegerField()
+
+
+
+class ResolveMarketSerializer(serializers.Serializer):
+
+    winner = serializers.ChoiceField(
+        choices=["YES","NO"]
+    )
--- core/views.py
+++ core/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .serializers import RegisterSerializer,LoginSerializer
+from .serializers import RegisterSerializer,LoginSerializer,DepositSerializer,WalletSerializer,MarketSerializer,MarketOutputSerializer,MarketDetailSerializer,PlaceOrderSerializer,MergeSerializer,ResolveMarketSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+from .models import Wallet,WalletTransaction,Market,Outcome,Position,Order
+from django.db import transaction
+from django.shortcuts import get_object_or_404
+from decimal import Decimal
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 
@@ -25,38 +32,6 @@ class RegisterAPIView(APIView):
         },status = status.HTTP_201_CREATED)
 
 
-# class LoginAPIView(APIView):
-
-#     def post(self,request):
-
-#         serializer = LoginSerializer(
-#             data = request.data
-#         )
-
-#         serializer.is_valid(raise_exception=True)
-
-#         user = authenticate(
-#             username = serializer.validated_data["username"],
-#             password = serializer.validated_data["password"]
-#         )
-
-#         if user is None:
-#             return Response({
-#                 "msg" : "invalid credentials"
-#             },status=status.HTTP_401_UNAUTHORIZED)
-        
-#         refresh = RefreshToken.for_user(user)
-#         refresh_token = str(refresh)
-#         access_token = str(refresh.access_token)
-
-#         return Response(
-#         {
-#         "msg" : "user signed in successfully",
-#         "access": access_token,
-#         "refresh": refresh_token
-#         },
-#         status=status.HTTP_200_OK
-#         )
 
         
 class LoginAPIView(APIView):
@@ -90,4 +65,490 @@ class LoginAPIView(APIView):
         status=status.HTTP_200_OK)
 
 
+
+
+class WalletAPIView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self,request):
+        serializer = DepositSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+
+        wallet = Wallet.objects.get(user = request.user)
+
+        with transaction.atomic():
+           wallet.available_balance = wallet.available_balance + serializer.validated_data["amount"]
+           wallet.save()
+
+           WalletTransaction.objects.create(
+              wallet = wallet,
+              transaction_type = "DEPOSIT",
+              amount = serializer.validated_data["amount"]
+        )
+
+        return Response({
+            "msg" : "money deposited successfully",
+            "wallet" : WalletSerializer(wallet).data
+        },
+        status = status.HTTP_200_OK)
+
+    def post(self,request):
+
+       serializer = DepositSerializer(data = request.data)
+       serializer.is_valid(raise_exception=True)
+       wallet = Wallet.objects.get(user = request.user)
+       if(wallet.available_balance < serializer.validated_data["amount"]):
+           return Response({
+               "msg" : "Insufficient balance"
+           },status=status.HTTP_400_BAD_REQUEST)
+
+       with transaction.atomic():
+            wallet.available_balance -= serializer.validated_data["amount"]
+            wallet.save()
+            WalletTransaction.objects.create(
+               wallet = wallet,
+               transaction_type = "WITHDRAW",
+               amount = serializer.validated_data["amount"]
+       )
+
+       return Response({
+           "msg" : "money withdrawn successfully",
+           "wallet" : WalletSerializer(wallet).data
+       },status=status.HTTP_200_OK)
+
+
+
+
+class MarketAPIView(APIView):
+
+    def post(self,request):
+
+        serializer = MarketSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        with transaction.atomic():
+           market =  Market.objects.create(
+               **serializer.validated_data
+           )
+
+           Outcome.objects.create(
+              market = market,
+              name = "YES"
+          )
+           Outcome.objects.create(
+              market = market,
+              name = "NO"
+           )
+             
+        return Response({
+            "msg" : "market created successfully"
+        },status=status.HTTP_201_CREATED)
+
+
+    def get(self,request):
+
+        markets = Market.objects.filter(status = "OPEN")
+
+        serializer = MarketOutputSerializer(markets,many = True)
+
+        return Response(
+            serializer.data
+        )
+
+    def get(self,request,pk):
+
+       market =  get_object_or_404(Market,id = pk)
+    
+       return Response(
+          MarketDetailSerializer(market).data
+        
+       )
+
+    def post(self, request, pk):
+
+        serializer = DepositSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        amount = serializer.validated_data["amount"]
+
+        market = get_object_or_404(Market, id=pk)
+
+        if market.status != "OPEN":
+            return Response(
+                {"msg": "Market is closed"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        wallet = Wallet.objects.get(user=request.user)
+
+        if wallet.available_balance < amount:
+            return Response(
+                {"msg": "Insufficient balance"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        yes_outcome = market.outcomes.get(name="YES")
+        no_outcome = market.outcomes.get(name="NO")
+
+        with transaction.atomic():
+
+            # Deduct wallet balance
+            wallet.available_balance -= amount
+            wallet.save()
+
+            # Wallet transaction
+            WalletTransaction.objects.create(
+                wallet=wallet,
+                transaction_type="SPLIT",
+                amount=amount,
+                description="Split into prediction shares",
+            )
+
+            # ---------- YES POSITION ----------
+
+            yes_position, created = Position.objects.get_or_create(
+                user=request.user,
+                outcome=yes_outcome,
+                defaults={
+                    "quantity": Decimal("0"),
+                    "average_price": Decimal("0.00"),
+                },
+            )
+
+            if created:
+                yes_position.quantity = amount
+                yes_position.average_price = Decimal("1.00")
+            else:
+                total_quantity = yes_position.quantity + amount
+
+                yes_position.average_price = (
+                    (yes_position.average_price * yes_position.quantity)
+                    + (Decimal("1.00") * amount)
+                ) / total_quantity
+
+                yes_position.quantity = total_quantity
+
+            yes_position.save()
+
+            # ---------- NO POSITION ----------
+
+            no_position, created = Position.objects.get_or_create(
+                user=request.user,
+                outcome=no_outcome,
+                defaults={
+                    "quantity": Decimal("0"),
+                    "average_price": Decimal("0.00"),
+                },
+            )
+
+            if created:
+                no_position.quantity = amount
+                no_position.average_price = Decimal("1.00")
+            else:
+                total_quantity = no_position.quantity + amount
+
+                no_position.average_price = (
+                    (no_position.average_price * no_position.quantity)
+                    + (Decimal("1.00") * amount)
+                ) / total_quantity
+
+                no_position.quantity = total_quantity
+
+            no_position.save()
+
+        return Response(
+            {
+                "message": "Split successful"
+            },
+            status=status.HTTP_200_OK,
+        )
+
+
+# {
+#     "outcome": "YES",
+#     "order_type": "BUY",
+#     "price": "7.50",
+#     "quantity": 20
+# }
+
+# class PlaceOrderAPIView(APIView):
+
+#     def post(self,request,pk):
+
+#         market = get_object_or_404(Market,id = pk)
+#         if(market.status != "OPEN"):
+#             return Response({
+#                 "msg" : "market is closed"
+#             })
+
+#         serializer = PlaceOrderSerializer(data = request.data)
+
+#         serializer.is_valid(raise_exception=True)
+
+#         price = serializer.validated_data["price"]
+#         quantity = serializer.validated_data["quantity"]
+#         orderType = serializer.validated_data["order_type"]
+#         outcome = serializer.validated_data["outcome"]
+
+#         wallet = Wallet.objects.get(user = request.user)
+
+#         if orderType == "BUY":
+
+#             if(wallet.available_balance < price * quantity ):
+#                 return Response({
+#                     "msg" : "insufficient balance"
+#                 })
+
+#             matching_orders = (
+#                 Order.objects.filter(
+#                 outcome = outcome,
+#                 order_type = "SELL",
+#                 status = "PENDING",
+#                 price__lte = price
+#                 )
+#             .order_by("price","created_at")
+#         )
+
+#         with transaction.atomic():
+
+#             wallet = Wallet.objects.get(user = request.user)
+
+#             wallet.locked_balance += price * quantity
+#             wallet.available_balance -= price * quantity
+
+#             wallet.save()
+
+#             WalletTransaction.objects.create(
+#                 wallet = wallet,
+#                 transaction_type = "LOCK",
+#                 amount = price * quantity
+#             )
+#             for order in matching_orders:
+#                 pass
+
+
+
+
+
+class MergeAPIView(APIView):
+
+  def post(self, request, pk):
+
+        serializer = MergeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        quantity = serializer.validated_data["quantity"]
+
+        market = get_object_or_404(Market, pk=pk)
+
+        if market.status != market.status.OPEN:
+            return Response(
+                {"msg": "Market is not open."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        positions = Position.objects.filter(
+            user=request.user,
+            outcome__market=market
+        )
+
+        yes_position = None
+        no_position = None
+
+        for position in positions:
+            if position.outcome.name == "YES":
+                yes_position = position
+            elif position.outcome.name == "NO":
+                no_position = position
+
+        if not yes_position or not no_position:
+            return Response(
+                {"msg": "You must own both YES and NO shares to merge."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if (
+            yes_position.quantity < quantity or
+            no_position.quantity < quantity
+        ):
+            return Response(
+                {"msg": "Insufficient shares to merge."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        wallet = get_object_or_404(Wallet, user=request.user)
+
+        with transaction.atomic():
+
+            yes_position.quantity -= quantity
+            no_position.quantity -= quantity
+
+            if yes_position.quantity == 0:
+                yes_position.delete()
+            else:
+                yes_position.save()
+
+            if no_position.quantity == 0:
+                no_position.delete()
+            else:
+                no_position.save()
+
+            wallet.available_balance += quantity
+            wallet.save()
+
+            WalletTransaction.objects.create(
+                wallet=wallet,
+                transaction_type="MERGE",
+                amount=quantity,
+                description=f"Merged {quantity} YES-NO pairs in '{market.title}'"
+            )
+
+        return Response(
+            {
+                "message": "Shares merged successfully."
+            },
+            status=status.HTTP_200_OK
+        )
+            
+            
+
+
+
+class ResolveMarketAPIView(APIView):
+
+    def post(self,request,pk):
+
+        serializer = ResolveMarketSerializer(
+            data = request.data
+        )
+
+        serializer.is_valid(raise_exception=True)
+
+        winner = serializer.validated_data["winner"]
+
+        market = get_object_or_404(Market,id = pk)
+
+
+        if(market.status != market.Status.CLOSED):
+            return Response({
+                "msg" : "only closed markets can be resolved."
+            })
+
+        with transaction.atomic():
+
+            market.status = market.Status.RESOLVED
+            market.resolved_at = timezone.now()
+            market.save()
+
+            outcome = market.outcomes.get(name = winner)
+            outcome.is_winner = True
+            outcome.save()
+
+
+        return Response({
+            "msg" : "market resolved successfully"
+        },status=status.HTTP_200_OK)
+
+
+
+class SettlementAPIView(APIView):
+
+    # def post(self,request,pk):
+
+    #     market = get_object_or_404(Market,id = pk)
+
+    #     if market.status != market.Status.RESOLVED:
+    #         return Response({
+    #             "msg" : "cannot settle because market is not resolved yet"
+    #         })
+
+    #     positions = Position.objects.filter(outcome__market = market)
+
+    #     for position in positions:
+            
+    #         if position.outcome.is_winner:
+    #             wallet = Wallet.objects.get(user = position.user)
+    #             wallet.available_balance +=  position.quantity
+    #             wallet.save()
+
+    #             WalletTransaction.objects.create(
+    #                 wallet = wallet,
+    #                 transaction_type = WalletTransaction.TransactionType.SETTLEMENT,
+    #                 amount =  position.quantity,
+    #                 description = "Settlement"
+    #             )
+
+    #         position.delete()
+
+    #     return Response({
+    #         "msg" : "market settled successfully"
+    #     })
+
+    def post(self,request,pk):
+
+        market = get_object_or_404(Market,id = pk)
+
+        if market.status != market.Status.RESOLVED:
+            return Response({
+                "msg" : "cannot settle because market is not resolved yet"
+            })
+
+        positions = Position.objects.select_related("user","outcome").filter(outcome__market = market)
+
+        with transaction.atomic():
+            for position in positions:
+            
+                if position.outcome.is_winner:
+                    wallet = Wallet.objects.get(user = position.user)
+                    wallet.available_balance +=  position.quantity
+                    wallet.save()
+
+                    WalletTransaction.objects.create(
+                       wallet = wallet,
+                       transaction_type = WalletTransaction.TransactionType.SETTLEMENT,
+                       amount =  position.quantity,
+                       description=f"Settlement for '{market.title}'"
+                    )
+                position.delete()
+
+        return Response({
+            "msg" : "market settled successfully"
+        })
+
+       
+
+
+        
+
+
+
+
+
+
+
+
+
+
+        
+
+        
+
+
+
+
+
+
+
+
+
+       
+
+
+
+        
+
+    
+
+
+        
 
